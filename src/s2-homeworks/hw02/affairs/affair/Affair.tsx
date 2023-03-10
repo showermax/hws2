427d617edc341a7630c292ebe3a80bc1d8d953e4
showermax <sevenhauz@gmail.com>
2023-03-10 16:28:23 +0300

    HW2 * 10 - в файле Affair.tsx дописать функции deleteCallback и использовать * 11 - в файле Affair.tsx отобразить приходящие данные

--- src/s2-homeworks/hw02/affairs/affair/Affair.tsx
+++ src/s2-homeworks/hw02/affairs/affair/Affair.tsx
@@ -10,8 +10,8 @@ type AffairPropsType = {
 }
 
 function Affair(props: AffairPropsType) {
-    const deleteCallback = () => {
-        // need to fix
+    const deleteCallback = (_id: number) => {
+       props.deleteAffairCallback(_id) // need to fix
     }
 
     const nameClass = s.name + ' ' + s2[props.affair.priority]
@@ -24,7 +24,7 @@ function Affair(props: AffairPropsType) {
             className={affairClass}
         >
             <div id={'hw2-name-' + props.affair._id} className={nameClass}>
-                {/*создаёт студент*/}
+                {props.affair.name/*создаёт студент*/}
 
                 {/**/}
             </div>
@@ -34,7 +34,7 @@ function Affair(props: AffairPropsType) {
 
             <button
                 id={'hw2-button-delete-' + props.affair._id}
-                className={buttonClass}
+                className={buttonClass} onClick={() => deleteCallback(props.affair._id)}
                 // need to fix
 
             >
