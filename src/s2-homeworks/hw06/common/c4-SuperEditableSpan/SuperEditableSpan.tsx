17741e86a45c8b393a482d83b450f9617b7de458
showermax <sevenhauz@gmail.com>
2023-04-07 19:43:11 +0300

    HW5 * 1 - в файле SuperEditableSpan.tsx дописать логику функций onEnterCallback, onBlurCallback, onDoubleClickCallBack

--- src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
+++ src/s2-homeworks/hw06/common/c4-SuperEditableSpan/SuperEditableSpan.tsx
@@ -42,19 +42,20 @@ const SuperEditableSpan: React.FC<SuperEditableSpanType> = (
 
     const onEnterCallback = () => {
         // выключить editMode при нажатии Enter // делают студенты
-
-        onEnter?.()
+      onEnter ?
+        onEnter() :
+          setEditMode(false)
     }
     const onBlurCallback = (e: React.FocusEvent<HTMLInputElement>) => {
         // выключить editMode при нажатии за пределами инпута // делают студенты
-
+        setEditMode(false)
         onBlur?.(e)
     }
     const onDoubleClickCallBack = (
         e: React.MouseEvent<HTMLSpanElement, MouseEvent>
     ) => {
         // включить editMode при двойном клике // делают студенты
-
+        setEditMode(true)
         onDoubleClick?.(e)
     }
 
