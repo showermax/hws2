ff6bc6f79f24d52e60b97a04cc572d9bededf47e
showermax <sevenhauz@gmail.com>
2023-04-15 22:35:15 +0300

    HW7 * 1 - в файле SuperSelect.tsx дописать логику функции onChangeCallback

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -36,7 +36,7 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
         : [] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        // onChangeOption(e.currentTarget.value)
+        if (onChangeOption) onChangeOption(e.currentTarget.value)
     }
 
     const finalSelectClassName = s.select + (className ? ' ' + className : '')
