4a217b5f519139379a5a8e702e911c1c74f1c95d
showermax <sevenhauz@gmail.com>
2023-04-18 21:59:02 +0300

    HW8 1streducer

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -51,7 +51,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
                       type={'radio'}
                       checked={o.id == value}
                       value={o.id}
-                      name={o.value}
+                      name={name}
                       // name, checked, value делают студенты
 
                       onChange={onChangeCallback}
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,11 +4,15 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: any) => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            return  (
+                action.payload ==='up' ? state.map(el=>({...el})).sort()
+                    :
+                    state.map(el=>({...el}))
+            )
+             // need to fix
         }
         case 'check': {
 
