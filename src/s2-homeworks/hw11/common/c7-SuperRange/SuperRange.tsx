960475a5fa9e5782a2353d910fe3c50e3ce757c7
showermax <sevenhauz@gmail.com>
2023-05-17 12:23:05 +0300

    HW11 * 3 - styles

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -5,7 +5,21 @@ const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
             sx={{ // стили для слайдера // пишет студент
-                
+
+                color: '#52af77',
+                height: 8,
+                width: 150,
+                '& .MuiSlider-track': {
+                border: 'none',
+            },
+                '& .MuiSlider-thumb': {
+                height: 18,
+                width: 18,
+                backgroundColor: '#fff',
+                border: '1px solid currentColor',
+
+            },
+
             }}
             {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
         />
