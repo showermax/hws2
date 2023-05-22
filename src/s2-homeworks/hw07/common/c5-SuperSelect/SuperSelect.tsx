cc667b57771fdc8e1d0ef6192f6011797ee369c9
showermax <sevenhauz@gmail.com>
2023-05-22 23:19:54 +0300

    * 4 - передать пропсы в SuperSelect

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -36,7 +36,8 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
         : [] // map options with key
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
-        if (onChangeOption) onChangeOption(e.currentTarget.value)
+        if (onChangeOption) onChangeOption(+e.currentTarget.value)
+        console.log(+e.currentTarget.value)
 
     }
 
