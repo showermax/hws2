bc464b1081e7c634634290f2eafd6bcc845fb01a
showermax <sevenhauz@gmail.com>
2023-06-06 14:00:36 +0200

    HW13 - * 1 - дописать функцию send

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -24,18 +24,20 @@ const HW13 = () => {
         const url =
             x === null
                 ? 'https://xxxxxx.ccc' // имитация запроса на не корректный адрес
-                : 'https://incubator-personal-page-back.herokuapp.com/api/3.0/homework/test'
+                : 'https://samurai.it-incubator.io/api/3.0/homework/test'
 
         setCode('')
         setImage('')
         setText('')
         setInfo('...loading')
-
         axios
             .post(url, {success: x})
             .then((res) => {
+                console.log(res)
                 setCode('Код 200!')
                 setImage(success200)
+                setText('...всё ок) код 200 - обычно означает что скорее всего всё ок)')
+                setInfo('')
                 // дописать
 
             })
