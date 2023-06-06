f2f53b7a5e8a04bd3d8171346d4a872bfdd6c47f
showermax <sevenhauz@gmail.com>
2023-06-06 14:53:27 +0200

    HW13 - * 2 - дизэйблить кнопки пока идёт запрос

--- src/s2-homeworks/hw13/HW13.tsx
+++ src/s2-homeworks/hw13/HW13.tsx
@@ -70,6 +70,7 @@ const HW13 = () => {
                         id={'hw13-send-true'}
                         onClick={send(true)}
                         xType={'secondary'}
+                        disabled={info==='...loading'}
                         // дописать
 
                     >
@@ -79,6 +80,7 @@ const HW13 = () => {
                         id={'hw13-send-false'}
                         onClick={send(false)}
                         xType={'secondary'}
+                        disabled={info==='...loading'}
                         // дописать
 
                     >
@@ -88,6 +90,7 @@ const HW13 = () => {
                         id={'hw13-send-undefined'}
                         onClick={send(undefined)}
                         xType={'secondary'}
+                        disabled={info==='...loading'}
                         // дописать
 
                     >
@@ -97,6 +100,7 @@ const HW13 = () => {
                         id={'hw13-send-null'}
                         onClick={send(null)} // имитация запроса на не корректный адрес
                         xType={'secondary'}
+                        disabled={info==='...loading'}
                         // дописать
 
                     >
